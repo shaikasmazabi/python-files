2a5b5f2b02a56b21e5dac183cf24934e1fee6911
shaikasmazabi <43025852+shaikasmazabi@users.noreply.github.com>
2018-09-22 11:23:32 +0530

    Add files via upload

---
+++ saz.py
@@ -0,0 +1,6 @@
+#printing name
+print("shaikasmazabi")
+print("Electronics and Communication Branch")
+print("Python lab")
+print("Maths is my favorite subject")
+print("White, Pink , Orange is my favorite color")
