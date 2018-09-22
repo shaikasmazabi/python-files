35b17cb775f49f0637fdcc92ceb5ab0c6687514b
shaikasmazabi <43025852+shaikasmazabi@users.noreply.github.com>
2018-09-22 11:20:10 +0530

    Add files via upload

---
+++ mato.py
@@ -0,0 +1,15 @@
+x=int(input("Enter a number :  "))
+y=int(input("Enter another number :  "))
+print(x, "+" ,y, "=" ,x+y)
+print(x, "-" ,y, "=" ,x-y)
+print(x, "*" ,y, "=" ,x*y)
+print(x, "%" ,y, "=" ,x%y)
+print(x, "/" ,y, "=" ,x/y)
+print(x, "**" ,y, "=" ,x**y)
+z=int(input("Enter your favorite number :  "))
+print("Your favorite number is " ,z)
+
+
+
+
+
